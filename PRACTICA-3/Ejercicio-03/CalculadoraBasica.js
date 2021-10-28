677cf8db5a7c8a4a21f9168ce153dc49db93d78c
Noé Fernández Moro <57045860+UO251683@users.noreply.github.com>
2021-10-28 12:54:51 +0200

    Add files via upload

--- PRACTICA-3/Ejercicio-03/CalculadoraBasica.js
+++ PRACTICA-3/Ejercicio-03/CalculadoraBasica.js
@@ -1,3 +1,17 @@
-function myfunction() {   
-    alert("how are you"); 
+function suma(primero, segundo) {   
+    alert(primero + segundo); 
 } 
+
+function resta(primero, segundo) {
+    if (primero >= segundo) {
+        alert(primero - segundo);
+    } else { alert("El primero no puede ser mayor que el segundo") }
+}
+
+function multiplicacion(primero, segundo) {
+    alert(primero * segundo);
+}
+
+function division(primero, segundo) {
+    alert(primero / segundo);
+}
